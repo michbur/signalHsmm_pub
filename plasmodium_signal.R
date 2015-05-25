8b9464874f819b4f4b4028bdd405691cccce3fcd
michbur <michalburdukiewicz@gmail.com>
2015-05-25 14:34:25 +0200

    prepared data for Piotr

--- plasmodium_signal.R
+++ plasmodium_signal.R
@@ -24,19 +24,38 @@ library(signalHsmm)
 library(seqinr)
 library(hmeasure)
 
+# signal.plas <- train_hsmm(read_uniprot(paste0(pathway, "plasmodium_big.txt"), euk = TRUE),
+#                           aaaggregation)
+
+#train algotrithm
 signal.hsmm1987 <- train_hsmm(read_uniprot(paste0(pathway, "pos_ultrahard_data.txt"), euk = TRUE),
                               aaaggregation)
-plas_pos <- read.fasta("plas_pos.fasta")
-plas_neg <- read.fasta("plas_neg.fasta")
+
+plas_pos <- read_uniprot("plasmodium_pos.txt", euk = TRUE, what = "signal")
+plas_neg <- read.fasta("plas_neg.fasta", seqtype = "AA")
+
+write.fasta(plas_pos, names(plas_pos), file = "plasmodium_pos_filtered.fasta")
 
 system("/home/michal/signalp-4.1/signalp -t euk -f short plas_neg.fasta > plas_neg.short_out")
-system("/home/michal/signalp-4.1/signalp -t euk -f short plas_pos.fasta > plas_pos.short_out")
+system("/home/michal/signalp-4.1/signalp -t euk -f short plasmodium_pos_filtered.fasta > plas_pos.short_out")
 predSignal <- rbind(read_signalp41("plas_pos.short_out"), 
       read_signalp41("plas_neg.short_out"))
 
 signal.hsmm1987_preds <- pred2df(predict(signal.hsmm1987, c(plas_pos, plas_neg)))
+signal.hsmm_preds <- pred2df(run_signalHsmm(c(plas_pos, plas_neg)))
 
+real_labels <- c(rep(1, length(plas_pos)), rep(0, length(plas_neg)))
 
-metrics <- do.call(rbind, lapply(list(signalP = predSignal, signalHsmm = signal.hsmm1987_preds), function(predictor)
-  HMeasure(c(rep(1, length(plas_pos)), rep(0, length(plas_neg))), predictor[["sp.probability"]])[["metrics"]]))
+metrics <- do.call(rbind, lapply(list(signalP = predSignal, signalHsmm = signal.hsmm1987_preds,
+                                      signalHsmm1987 = signal.hsmm1987_preds), function(predictor)
+  HMeasure(real_labels, predictor[["sp.probability"]])[["metrics"]]))
 #wow!
+
+#library(pROC)
+#auc(real_labels, signal.hsmm_preds[["sp.probability"]])
+HMeasure(real_labels, signal.hsmm_preds[["sp.probability"]])[["metrics"]]
+
+signalHSMM_proteins <- plas_pos[(signal.hsmm_preds[["sp.probability"]] > 0.5 & !predSignal[["sp.probability"]])[as.logical(real_labels)]]
+regions <- sapply(signalHSMM_proteins,
+       function(single_protein) find_nhc(single_protein, attr(single_protein, "sig")))
+save(signalHSMM_proteins, regions, file = "dane_dla_Piotra.RData")
