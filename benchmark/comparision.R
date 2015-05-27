a4382c931e0c25221a58ec6d656b9b827e3c4c60
michbur <michalburdukiewicz@gmail.com>
2015-05-27 17:57:15 +0200

    added benchmark for plasmodium

---
+++ benchmark/comparision.R
@@ -0,0 +1,74 @@
+read_signalp41 <- function(connection) {
+  all_lines <- readLines(connection)[-c(1L:2)]
+  do.call(rbind, lapply(all_lines, function(i) {
+    line <- strsplit(i, " ")[[1]]
+    line <- line[!line == ""]
+    res <- data.frame(sp.probability = line[10] == "Y",
+                      sp.start = ifelse(line[10] == "Y", 1, NA),
+                      sp.end = ifelse(line[10] == "Y", as.numeric(line[5]) - 1, NA))
+    rownames(res) <- line[1]
+    res
+  }))
+}
+
+read_predsi <- function(connection) {
+  dat <- read.table(connection, sep = "\t")
+  data.frame(sp.probability = dat[, 4] == "Y",
+             sig.start = ifelse(dat[, 4] == "Y", 1, NA),
+             sig.end = ifelse(dat[, 4] == "Y", as.numeric(dat[, 3]), NA),
+             row.names = dat[, 1])
+}
+
+read_phobius <- function(connection) {
+  all_lines <- readLines(connection)
+  all_lines <- all_lines[-1]
+  splited <- strsplit(all_lines, " ")
+  #remove "" characters
+  purged <- t(sapply(splited, function(i) i[i != ""]))
+  cl_sites <- sapply(which(purged[, 3] == "Y"), function(i)
+    as.numeric(strsplit(strsplit(purged[i,4], "/")[[1]][1], "c")[[1]][[2]]))
+  res <- data.frame(sp.probability = purged[, 3] == "Y",
+                    sig.start = ifelse(purged[, 3] == "Y", 1, NA),
+                    sig.end = rep(NA, nrow(purged)), 
+                    row.names = purged[, 1])
+  res[purged[, 3] == "Y", "sig.end"] <- cl_sites
+  res
+}
+
+read_philius <- function(connection) {
+  require(XML)
+  all_dat <- xmlToList(xmlTreeParse(connection, asTree = TRUE))
+  seq_dat_id <- 1L:(length(all_dat)/2)*2
+  #data for table
+  table_dat <- sapply(seq_dat_id, function(i) 
+    unlist(all_dat[i][[1]][[1]][c(24, 22)]))
+  cleaved <- sapply(table_dat, function(i)
+    !(is.null(i[1]) || is.na(i[1])))
+  res <- data.frame(sp.probability = cleaved,
+                    sig.start = ifelse(cleaved, 1, NA),
+                    sig.end = rep(NA, length(seq_dat_id)),
+                    row.names = unlist(all_dat[1L:(length(all_dat)/2)*2 - 1]))
+  res[cleaved, "sig.end"] <- as.numeric(sapply(table_dat[cleaved], function(i) i[2])) - 1
+  res
+}
+
+library(signalHsmm)
+library(seqinr)
+library(hmeasure)
+
+
+signal.hsmm1987_preds <- pred2df(predict(signal.hsmm1987, c(plas_pos, plas_neg)))
+signal.hsmm_preds <- pred2df(run_signalHsmm(c(plas_pos, plas_neg)))
+
+real_labels <- c(rep(1, length(plas_pos)), rep(0, length(plas_neg)))
+
+metrics <- do.call(rbind, lapply(list(signalPnotm = read_signalp41("./benchmark/signaP41notm.txt"), 
+                                      signalPtm = read_signalp41("./benchmark/signaP41tm.txt"), 
+                                      predsi = read_predsi("./benchmark/predsi.txt"),
+                                      phobius = read_phobius("./benchmark/phobius.txt"),
+                                      signalHsmm = signal.hsmm1987_preds,
+                                      signalHsmm1987 = signal.hsmm1987_preds), function(predictor)
+                                        HMeasure(real_labels, predictor[["sp.probability"]])[["metrics"]]))
+
+
+write.table(metrics[, c("AUC", "H")], sep = "\t", file = "grant_short.txt")
--- plasmodium_signal.R
+++ plasmodium_signal.R
@@ -34,12 +34,19 @@ signal.hsmm1987 <- train_hsmm(read_uniprot(paste0(pathway, "pos_ultrahard_data.t
 plas_pos <- read_uniprot("plasmodium_pos.txt", euk = TRUE, what = "signal")
 plas_neg <- read.fasta("plas_neg.fasta", seqtype = "AA")
 
+short_plas_neg <- lapply(plas_neg, function(i)
+  i[1L:ifelse(length(i) > 500, 500, length(i))])
+
+write.fasta(c(plas_pos, short_plas_neg),
+            c(names(plas_pos), names(plas_neg)),
+            file = "plas_total.fasta")
+
 write.fasta(plas_pos, names(plas_pos), file = "plasmodium_pos_filtered.fasta")
 
 system("/home/michal/signalp-4.1/signalp -t euk -f short plas_neg.fasta > plas_neg.short_out")
 system("/home/michal/signalp-4.1/signalp -t euk -f short plasmodium_pos_filtered.fasta > plas_pos.short_out")
 predSignal <- rbind(read_signalp41("plas_pos.short_out"), 
-      read_signalp41("plas_neg.short_out"))
+                    read_signalp41("plas_neg.short_out"))
 
 signal.hsmm1987_preds <- pred2df(predict(signal.hsmm1987, c(plas_pos, plas_neg)))
 signal.hsmm_preds <- pred2df(run_signalHsmm(c(plas_pos, plas_neg)))
@@ -48,7 +55,7 @@ real_labels <- c(rep(1, length(plas_pos)), rep(0, length(plas_neg)))
 
 metrics <- do.call(rbind, lapply(list(signalP = predSignal, signalHsmm = signal.hsmm1987_preds,
                                       signalHsmm1987 = signal.hsmm1987_preds), function(predictor)
-  HMeasure(real_labels, predictor[["sp.probability"]])[["metrics"]]))
+                                        HMeasure(real_labels, predictor[["sp.probability"]])[["metrics"]]))
 #wow!
 
 #library(pROC)
@@ -57,5 +64,5 @@ HMeasure(real_labels, signal.hsmm_preds[["sp.probability"]])[["metrics"]]
 
 signalHSMM_proteins <- plas_pos[(signal.hsmm_preds[["sp.probability"]] > 0.5 & !predSignal[["sp.probability"]])[as.logical(real_labels)]]
 regions <- sapply(signalHSMM_proteins,
-       function(single_protein) find_nhc(single_protein, attr(single_protein, "sig")))
+                  function(single_protein) find_nhc(single_protein, attr(single_protein, "sig")))
 save(signalHSMM_proteins, regions, file = "dane_dla_Piotra.RData")
